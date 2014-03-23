b2d34f710bc6336778958508a94a9cfaf7ae21ba
Jan Berdel <Jan.Berdel@glueckkanja.com>
2014-03-23 22:11:05 +0100

    refactor

---
+++ EmbeddedRangeMessageProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace IsbnTools
+{
+    public class EmbeddedRangeMessageProvider : IRangeMessageProvider
+    {
+        public EmbeddedRangeMessageProvider(string resourceName = "IsbnTools.range.xml")
+        {
+            ResourceName = resourceName;
+        }
+
+        public string ResourceName { get; private set; }
+
+        public virtual RangeMessage GetRangeMessage()
+        {
+            using (Stream stream = GetType().Assembly.GetManifestResourceStream(ResourceName))
+            {
+                return new RangeMessage(XDocument.Load(stream));
+            }
+        }
+    }
+}
